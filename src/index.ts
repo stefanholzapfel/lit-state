5b9c33349906a7e86cf4116efb8da08e09deba84
stefanholzapfel <stefan@holzapfel-consulting.at>
2024-09-11 15:46:53 +0200

    Fix(types): Add StateEntryPath type

--- src/index.ts
+++ src/index.ts
@@ -42,9 +42,6 @@ export interface SetStateOptions<State> {
     entryPath?: any;
 }
 
-// TODO: type
-export type StateEntryPath = any;
-
 export interface SubscribeStateFromElementOptions extends SubscribeStateOptions {
     autoUnsubscribe?: boolean;
 }
@@ -62,6 +59,25 @@ export type StateChange<State> =
             [P in keyof State]?:
             State[P] | StateChange<State[P]>
         } & { _reducerMode?: StateReducerMode };
+
+type Digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15;
+type NextDigit = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 'STOP'];
+type Increment<Depth> = Depth extends Digit ? NextDigit[Depth] : 'STOP';
+
+export type IndexOrPredicateFunction = number | PredicateFunction<any>;
+export type StateEntryPath<Object, Path extends PropertyKey[] = [], Depth = 0> = Object extends object ?
+    Path |
+    // Check if depth > max allowed
+    Depth extends string ?
+            // ...if yes, don't typecheck deeper levels and allow everything (for performance reasons)
+            [...Path, ...any[]] :
+            // ...otherwise check if object is array
+            (Object extends readonly any[] ?
+                // ...when array only allow index or PredicateFunction
+                StateEntryPath<Object[number], [...Path, IndexOrPredicateFunction], Increment<Depth>>
+                // ...when object generate type of all possible keys
+                : { [Key in keyof Object]: StateEntryPath<Object[Key], [...Path, Key], Increment<Depth>> }[keyof Object])
+    : Path;
 
 export * from './litElementStateful';
 export * from './litElementState.service';
